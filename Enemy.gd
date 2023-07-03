fd0c7645fedf92e01e90085bcd3ddfd467922be6
TequilaJosh <friendsmatta@hotmail.com>
2023-07-03 14:46:27 -0500

    Added basic attacks from dragons, death process for enemies and started working on reanimation
    Still need to work on reanimation and soul siphoning

--- Enemy.gd
+++ Enemy.gd
@@ -1,29 +1,78 @@
 extends Area2D
 
 
-var speed = 2
-var attack_range = 250
-var attack_power = 5
+@export var speed = 20
+@export var attack_range = 250
+@export var attack_power = 5
+@export var health := 100
 var targeted
-#$hit_cooldown.wait_time = 1
-
-# Called when the node enters the scene tree for the first time.
+@onready var anim = $AnimatedSprite2D
+@onready var attack_delay = $hit_cooldown
+@onready var undead_targetting = $"Undead_targeting system"
+var state
+signal i_died
 func _ready():
-	pass
+	state = "attack"
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	if position.x < 1024 - attack_range:
-		position.x += speed
+	if position.y < 768:
+		position.y += speed * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		speed = 0
 
 
-func _on_sword_area_entered(area):
-	$hit_cooldown.start()
-	targeted = area
 
 func _on_hit_cooldown_timeout():
-	if targeted.broken == false:
-		targeted.health -= attack_power
-	else:
-		attack_range = 0
+	if state != "dead":
+		if targeted.broken == false:
+			targeted.take_damage(attack_power)
+		else:
+			attack_range = 0
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("wall"):
+		targeted = area
+		anim.play("Attack")
+		attack_delay.start()
+		speed = 0
+
+func take_damage(damage):
+	if health >= damage:
+		health -= damage
+		if health <= 0:
+			die()
+				
+				
+func die():
+	#Currency.dead_list.append(self.position)
+	state = "dead"
+	emit_signal("i_died")
+	anim.play("Dead")
+	attack_power = 0
+	speed = 0
+	rotation = 90
+
+
+#Risen state code here
+func raise():
+	state = "risen"
+	undead_targetting.disabled = false
+	# Code here for recovering stats from before death
+	
+	
+	# slow speed going up, they will target the nearest enemy until they die or the
+	#life timer ends
+	speed = -20
+	
+	
+func find_target():
+	var target_list = []
+	var possible_target_list = undead_targetting.get_overlapping_areas()
+	for target in possible_target_list:
+		if "enemy" in target.name:
+			target_list.append(target)
+	
+	
